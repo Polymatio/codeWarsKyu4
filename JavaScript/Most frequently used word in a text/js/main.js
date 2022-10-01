f23b2c5433bd86cf55dcd163abfd584ba0d04582
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2022-09-30 17:32:05 -0700

    update file

--- JavaScript/Most frequently used word in a text/js/main.js
+++ JavaScript/Most frequently used word in a text/js/main.js
@@ -1,12 +1,19 @@
 // Create a function that finds the most frequently used word in a text
+// Assigning regex to ".*[a-zA-Z].*"
+// Creating a conditional to to count new words
+// Assigning wordMap to be a new Map constructor to find a new word
+// Using a forEach method to pick each individual word
+// Using toLowerCase method to make the word count not be case sensitive
+// Assigning count to wordMap to count the words and using Map.get method to return the word
+// Using set method to store in the words and count
+// Assigning sortedSwordMap to a new Map using spread syntax to expand individual elements and using the key and sort method to arrange the words
+// assigning result to Array.from, keys, and filter method to help arrange the words
+// Conditional will be used for finding special characters in the word
+// Assigning response with replace method to help remove special characters in the word if necessary
+// It will return the filter of the results and make sure if it is not a response to be undefined else return an empty array
 
 function topThreeWords(text) {
     let regex = ".*[a-zA-Z].*";
-    
-    // Create a conditional Statement to record each word,
-    // Use a forEach function to save each word,
-    // Making sure it is not case senseitive,
-    // counting a word out of time.
 
     if(text.match(regex)) {
         let wordMap = new Map();
@@ -23,13 +30,7 @@ function topThreeWords(text) {
             }
         })
         
-        // Creating a const to sort the words in an entry,
-        // used to count the words individually.
-
         const sortedWordMap = new Map([...wordMap.entries()].sort((a, b) => b[1] - a[1]));
-
-        // Create a conditional statement using special characters,
-        // Respond to special characters and non special characters.
 
         let result = Array.from(sortedWordMap.keys()).filter( (word, index) => index < 3);
         result = result.map(response => {
